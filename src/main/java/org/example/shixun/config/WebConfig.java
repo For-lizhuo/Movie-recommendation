5f5613d4ff546d5a47aa3fba30a7b6114d924abe
zwh <599316149@qq.com>
2021-07-09 01:51:49 +0800

    跨域cookie

--- src/main/java/org/example/shixun/config/WebConfig.java
+++ src/main/java/org/example/shixun/config/WebConfig.java
@@ -23,7 +23,7 @@ public class WebConfig  extends WebMvcConfigurerAdapter{
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-				.allowedOrigins("http://localhost:8080")
+				.allowedOrigins("http://localhost:3000")
 				.allowCredentials(true);
 	}
 	
