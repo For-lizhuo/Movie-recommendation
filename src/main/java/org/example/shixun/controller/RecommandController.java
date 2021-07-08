880c1f362b4ff6311660ae2c90a80c6aa1ea28b4
ycstcxy <67366456+ycstcxy@users.noreply.github.com>
2021-07-08 09:53:01 +0800

    增加crossorigin跨域标签

--- src/main/java/org/example/shixun/controller/RecommandController.java
+++ src/main/java/org/example/shixun/controller/RecommandController.java
@@ -4,16 +4,14 @@ import org.example.shixun.domain.Movie;
 import org.example.shixun.domain.User;
 import org.example.shixun.service.RecommandService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.List;
 
 //@Controller
 @RestController
+@CrossOrigin(origins = "*",maxAge = 3600)
 public class RecommandController {
     @Autowired
     private RecommandService recommandService;
